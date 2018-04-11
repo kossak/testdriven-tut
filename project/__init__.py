d48b0928f6eec6b83b2db1afe5089ffd93834471
bkossakowski <bkossakowski@pgs-soft.com>
2018-04-11 11:39:43 +0200

    dockerized, tested, enhanced

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,33 @@
+import os
+
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong'
-    })
+db = SQLAlchemy(app)
+
+
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set conf
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    app.shell_context_processor({'app': app, 'db': db})
+    return app
